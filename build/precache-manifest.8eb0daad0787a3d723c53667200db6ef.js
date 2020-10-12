c86942a8f57f7d57b695be4ee2a84adcb4ff20dd
raghulselvam <72665462+raghulselvam@users.noreply.github.com>
2020-10-12 18:48:00 +0530

    Add files via upload

---
+++ build/precache-manifest.8eb0daad0787a3d723c53667200db6ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27396e81925f039e2618f537726c2415",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71c2e5abaa98d9db7323",
+    "url": "/static/css/main.83de825e.chunk.css"
+  },
+  {
+    "revision": "fc511c40343a5016f383",
+    "url": "/static/js/2.87ee597e.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/static/js/2.87ee597e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71c2e5abaa98d9db7323",
+    "url": "/static/js/main.ace0a8dd.chunk.js"
+  },
+  {
+    "revision": "ea049a9b5aa085bfefe9",
+    "url": "/static/js/runtime-main.fe0cf552.js"
+  }
+]);
